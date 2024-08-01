1a863b29165b5931232f35448c12a944014ae510
Leo <130664816+leoGEMIDA@users.noreply.github.com>
2024-08-01 11:42:39 -0300

    a

---
+++ 02_variaveis.py
@@ -0,0 +1,17 @@
+# Variáveis
+nome = "Gisele"
+idade = 44
+
+#Exibição de valores
+print(nome)
+print(idade)
+print("anos.\n")
+
+# 1° método de concatenação (string: str)
+print("o meu nome é: " + nome + " e tenho " + str(idade) + " anos.\n")
+
+# 2° método de concatenação (format)
+print("o meu nome é: {} e tenho {}  anos.\n".format(nome,idade))
+
+#3° método de concatenação (f strimg)
+print(f"o meu nome é: {nome} e tenho {idade} anos.\n" )
