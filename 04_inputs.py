bd7960a7487f62d16e8aff95056e8d67620b013d
Leo <130664816+leoGEMIDA@users.noreply.github.com>
2024-08-01 12:36:08 -0300

    a

---
+++ 04_inputs.py
@@ -0,0 +1,10 @@
+#Armazenando valores/ Inputs
+print("infome os valores para multiplicação\n")
+n1 =float(input("informe o 1° numero: "))
+n2 =float(input("informe o 2° numero: "))
+
+# Cálculo
+mult = n1 * n2
+
+# Exibir resultado
+print("O resultado da multiplicação é: {}.\n".format(mult))
---
+++ 07_replace.py
@@ -0,0 +1,8 @@
+# Variável
+mensagem = "Eu gosto de java!"
+
+# Nova variável que recebe o método upper
+nova_mensagem = mensagem.replace("Java", "python")
+
+# Exibir
+print(nova_mensagem)
